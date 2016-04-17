ae262290bb57b267a84ae0e9a156284db9078cbd
CLIN <linchenyin@gmail.com>
2016-04-17 11:37:59 -0700

    Add menu

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include( "views/head.phtml");?>
+<body role="document">
+<?php include( "views/navbar.phtml");?>
+<div class="container theme-showcase" role="main">
+    
+<?php include( "views/container.phtml");?>
+</div><!-- /container -->
+<?php include( "views/bootstrapjs.phtml");?>
+</body>
+</html>
