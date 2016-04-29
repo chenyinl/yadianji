92139200ba9b7ea64363c0e4d4f57d22e2b2d6e6
CLIN <linchenyin@gmail.com>
2016-04-28 21:35:30 -0700

    Add pics

---
+++ k-lady-fashion.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include( "views/head.phtml");?>
+<body role="document">
+<?php include( "views/navbar.phtml");?>
+<div class="container theme-showcase" role="main">
+    
+<?php include( "views/kladyfashion.phtml");?>
+
+</div><!-- /container -->
+<?php include( "views/bootstrapjs.phtml");?>
+</body>
+</html>
