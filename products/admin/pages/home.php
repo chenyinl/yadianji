6399e0ba3eb778d9c74d4aa8d4e6691c24a67570
CLIN <linchenyin@gmail.com>
2017-03-13 10:09:42 -0700

    Add cost on the main list

--- products/admin/pages/home.php
+++ products/admin/pages/home.php
@@ -120,6 +120,7 @@ function ValidateSubmit(form)
               <th width="60"><?php echo $this->texts["sku"];?></th>
 			  <th><?php echo $this->texts["description"];?></th>
               <th width="60"><?php echo $this->texts["retailprice"];?></th>
+              <th width="60"><?php echo $this->texts["cost"];?></th>
 			  <th width="60"><?php echo $this->texts["price"];?></th>
               <th width="60"><?php echo $this->texts["quantity"];?></th>
 			  <th width="60"><?php echo $this->texts["delete"];?></th>
@@ -161,6 +162,7 @@ function ValidateSubmit(form)
                 <td><?php echo $listing->sku;?></td>
 				<td><?php echo $this->text_words($listing->description,60);?></td>
                 <td><?php echo $this->settings["website"]["currency"].$listing->retailprice;?></td>
+                <td><?php echo $this->settings["website"]["currency"].$listing->cost;?></td>
 				<td><?php echo $this->settings["website"]["currency"].$listing->price;?></td>
                 <td><?php echo $listing->quantity;?></td>
 				<td><input type="checkbox" value="<?php echo $i;?>" name="delete_listings[]"/></td>
